58a759fb54d035040f91a4e19c18870fb4c06939
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-01-28 19:37:39 -0500

    Update camera_gpu.cpp

--- camera_gpu.cpp
+++ camera_gpu.cpp
@@ -1,6 +1,5 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
-//#include <opencv2/imgproc.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/cudaimgproc.hpp>
